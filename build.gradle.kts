8f35424f4c5a6036fe31980b0f0c738b15d86800
Christian Klem <christian.klem@nav.no>
2025-12-08 13:14:46 +0100

    Oppdater lz4-java til 1.10.1
     - Legg til at.yawk.lz4 som en eksplisitt dependency da 1.10.1 ikke er i mavencentral ennå
     - Exclude org.lz4 fra kafka-clients

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,31 +13,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.kafka:kafka-clients:4.1.0")
+    // Kafka
+    implementation("at.yawk.lz4:lz4-java:1.10.1")
+    implementation("org.apache.kafka:kafka-clients:4.1.0") {
+        // "Fikser CVE-2025-12183 - lz4-java >1.8.1 har sårbar versjon (transitive dependency fra kafka-clients:4.1.0)"
+        exclude("org.lz4", "lz4-java")
+    }
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
     implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:$opentelemetryLogbackMdcVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.2.21")
-
-    constraints {
-        implementation("org.lz4:lz4-java") {
-            modules {
-                module("org.lz4:lz4-java") {
-                    replacedBy("at.yawk.lz4:lz4-java", "Fork of the original unmaintained lz4-java library that fixes a CVE")
-                }
-            }
-            version {
-                require("1.8.1")
-            }
-            because(
-                "Fikser CVE-2025-12183 - lz4-java 1.8.0 har sårbar versjon (transitive dependency fra kafka-clients:4.1.0)",
-            )
-        }
-    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        dependsOn(installDist)
+        useJUnitPlatform()
+    }
 }
 
 kotlin {
