4a3a168101612eed958bb22a32ffd2b40130788b
nimajimale <nima0512@gmail.com>
2026-04-23 13:51:49 +0200

    Fiks sårbarhet i logstash-logback.encoder

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
 
     constraints {
         implementation("tools.jackson.core:jackson-core") {
-            version { require("3.1.0") }
-            because("versjoner < 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
+            version { require("3.1.1") }
+            because("versjoner <= 3.1.0 har sårbarhet. inkludert i logstash-logback-encoder:9.0")
         }
     }
 }
